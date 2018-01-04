42977cf05b4226a715ae208e906aefdb6798a7e5
Tobias Dély <cleverhatcamouflage@gmail.com>
2018-01-04 19:12:28 +0100

    Improve folder structure for namespace purposes

--- example/reference/private/config/autoloader.php
+++ example/reference/private/config/autoloader.php
@@ -6,9 +6,6 @@ $loader = new PhalconLoader();
 
 $loader->registerNamespaces(array(
     'Freischutz' => LIB_DIR,
-    'Freischutz\Application' => LIB_DIR . '/application',
-    'Freischutz\Security' => LIB_DIR . '/security',
-    'Freischutz\Utility' => LIB_DIR . '/utility',
     'Reference\Controllers' => APP_DIR . '/controllers',
     'Reference\Models' => APP_DIR . '/models',
 ));
--- test/integration/_shared/config/autoloader.php
+++ test/integration/_shared/config/autoloader.php
@@ -6,9 +6,6 @@ $loader = new PhalconLoader();
 
 $loader->registerNamespaces(array(
     'Freischutz' => $libDir,
-    'Freischutz\Application' => $libDir . '/application',
-    'Freischutz\Security' => $libDir . '/security',
-    'Freischutz\Utility' => $libDir . '/utility',
     'Test\Controllers' => $appDir . '/controllers',
     'Test\Models' => $appDir . '/models',
 ));

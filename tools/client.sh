4cd06dca98cb982cd5b1d8aa68d84e1d78a89795
Tobias Dély <cleverhatcamouflage@gmail.com>
2018-09-19 00:52:35 +0200

    Add licensing information to client.sh

--- tools/client.sh
+++ tools/client.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#
+# Simple CLI client for interacting with Freischutz RESTful APIs
+#
+# see       https://gitlab.com/tdely/freischutz/ Freischutz on GitLab
+#
+# author    Tobias Dély (tdely) <cleverhatcamouflage@gmail.com>
+# copyright 2017-present Tobias Dély
+# license   https://directory.fsf.org/wiki/License:BSD-3-Clause BSD 3-clause "New" or "Revised" License
+#
+
 set -o errexit
 set -o pipefail
 set -o nounset
